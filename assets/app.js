e974728854424c3d5fea5f0a9e015ccb498f72d5
slieman <slieman2001@hotmail.com>
2020-03-25 11:51:29 +0200

    up.

--- assets/app.js
+++ assets/app.js
@@ -3,11 +3,18 @@ const output  = document.querySelector('p');
 
 function trackUserHandler() {
 console.log('Clicked');
-navigator.geolocation.getCurrentPosition(posData => {
-    console.log(posData);
+navigator.geolocation.getCurrentPosition(
+    posData => {
+        setTimeout(() => {
+            console.log(posData);
+        }, 2000);
 },err => {
     console.log(err);
 });
+setTimeout(() => {
+    console.log('Timer done');
+    
+}, 0);
 console.log('Getting position ...');
 };
 
