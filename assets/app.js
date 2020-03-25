fe26cff9b27e6ab850aaacd09c47f3e42e4be1a5
slieman <slieman2001@hotmail.com>
2020-03-25 11:57:22 +0200

    use promise

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,28 @@
 const button  = document.querySelector('button');
 const output  = document.querySelector('p');
 
+const setTimer = (duration) => {
+    const promise = new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        }, duration);
+    });
+    return promise;
+}
+
 function trackUserHandler() {
-console.log('Clicked');
+// console.log('Clicked');
 navigator.geolocation.getCurrentPosition(
     posData => {
-        setTimeout(() => {
-            console.log(posData);
-        }, 2000);
+       setTimer(2000).then(data=> {
+           console.log(data,posData); 
+       });
 },err => {
     console.log(err);
 });
-setTimeout(() => {
+setTimer(1000).then(()=>{
     console.log('Timer done');
-    
-}, 0);
+})
 console.log('Getting position ...');
 };
 
@@ -28,4 +36,4 @@ for(let i=0; i<1000000; i++) {
     result +=i;
 }
 
-console.log(result);
+// console.log(result);
